cc8802d95775e9d7d1aaf6007a9a8520fb42d13c
caleb <calebjm288@ksu.edu>
2022-11-14 11:38:46 -0600

    Fixed the team season summary query and added the parameters for the procedure

--- NBAStatsData/Sql/Procedures/NBA.RetrieveTeamSeasonSummaries.sql
+++ NBAStatsData/Sql/Procedures/NBA.RetrieveTeamSeasonSummaries.sql
@@ -1,65 +1,47 @@
 ﻿CREATE OR ALTER PROCEDURE NBA.RetrieveTeamSeasonSummaries
-	@TeamName NVARCHAR(128) = '%'
+	@TeamName NVARCHAR(128) = '%',
+	@WinsMin INT = 0,
+	@WinsMax INT = 1000,
+	@LossesMin INT = 0,
+	@LossesMax INT = 1000,
+	@PPGScoredMin DECIMAL(7,1) = 0,
+	@PPGScoredMax DECIMAL(7,1) = 1000,
+	@PPGAgainstMin DECIMAL(7,1) = 0,
+	@PPGAgainstMax DECIMAL(7,1) = 1000,
+	@GamesPlayedMin INT = 0,
+	@GamesPlayedMax INT = 1000
 AS
-WITH SourceCTE AS (
-SELECT IIF(HomeTeam.HomeScore > AwayTeam.AwayScore, 1, 0) as HomeWins,
-            IIF(HomeTeam.HomeScore < AwayTeam.AwayScore, 1, 0) as AwayWins, HomeTeam.GameID,
-			HomeTeam.HomeID, AwayTeam.AwayID, HomeTeam.HomeScore, AwayTeam.AwayScore
-FROM (
-	SELECT  TG.GameID, TG.TeamID AS HomeID, TG.Score AS HomeScore
-	FROM NBA.TeamGame TG
-	WHERE TG.TeamTypeID = 1
-	)  HomeTeam
-	INNER JOIN 
+WITH CTE as (
+SELECT G.GameID, TG.TeamID, TG.Score,
 	(
-	SELECT  TG.GameID, TG.TeamID AS AwayID, TG.Score AS AwayScore
-	FROM NBA.TeamGame TG
-	WHERE TG.TeamTypeID = 2
-	) AwayTeam ON HomeTeam.GameID = AwayTeam.GameID
-	)
-SELECT T.[Name], SUM(HomeWins) + 
-		(
-		SELECT SUM(cte2.AwayWins)
-		FROM SourceCTE cte2
-		WHERE T.TeamID = cte2.AwayID
-		) as Wins,
-		(Count(*) + (
-		SELECT COUNT(*)
-		FROM SourceCTE cte2
-		WHERE T.TeamID = cte2.AwayID
-		)) - (SUM(HomeWins) + 
-		(
-		SELECT SUM(cte2.AwayWins)
-		FROM SourceCTE cte2
-		WHERE T.TeamID = cte2.AwayID)
-		) as Losses,
-		CAST((SUM(HomeScore) + 
-		(
-		SELECT SUM(cte2.AwayScore)
-		FROM SourceCTE cte2
-		WHERE T.TeamID = cte2.AwayID
-		)) / (Count(*) + 0.0+(
-		SELECT COUNT(*)
-		FROM SourceCTE cte2
-		WHERE T.TeamID = cte2.AwayID
-		)) AS DECIMAL(7,1)) as AveragePointsScored,
-		CAST(
-		(SUM(AwayScore) + 
-		(
-		SELECT SUM(cte2.HomeScore)
-		FROM SourceCTE cte2
-		WHERE T.TeamID = cte2.AwayID
-		)) / (Count(*) + 0.0+ (
-		SELECT COUNT(*)
-		FROM SourceCTE cte2
-		WHERE T.TeamID = cte2.AwayID
-		)) AS DECIMAL(7,1))  as AveragePointsAgainst,
-		(Count(*) + (
-		SELECT COUNT(*)
-		FROM SourceCTE cte2
-		WHERE T.TeamID = cte2.AwayID
-		)) as GamesPlayed
-FROM SourceCTE INNER JOIN NBA.Team T ON SourceCTE.HomeID = T.TeamID
-WHERE T.[Name] LIKE @TeamName
-GROUP BY T.TeamID, T.[Name]
-ORDER BY Wins DESC;
+		SELECT TG1.Score
+		FROM NBA.Game G1 INNER JOIN NBA.TeamGame TG1 ON TG1.GameID = G1.GameID
+		WHERE G1.GameID = G.GameID AND TG1.TeamTypeID <> TG.TeamTypeID
+	) as OppScore,
+	(
+		SELECT TG1.TeamID
+		FROM NBA.Game G1 INNER JOIN NBA.TeamGame TG1 ON TG1.GameID = G1.GameID
+		WHERE G1.GameID = G.GameID AND TG1.TeamTypeID <> TG.TeamTypeID
+	) as OppID
+FROM NBA.Game G INNER JOIN NBA.TeamGame TG ON G.GameID = TG.GameID
+),
+CTE2 AS(
+SELECT CTE.TeamID, 
+		T.[Name],
+		SUM(IIF(CTE.Score > CTE.OppScore, 1, 0)) AS Wins, 
+		SUM(IIF(CTE.Score > CTE.OppScore, 0, 1)) AS Losses,
+		CAST(AVG(CTE.Score+0.0) AS DECIMAL(7,1)) AS PPGScored,
+		CAST(AVG(CTE.OppScore+0.0) AS DECIMAL(7,1)) AS PPGAgainst,
+		COUNT(CTE.TeamID) AS GamesPlayed
+FROM CTE INNER JOIN NBA.Team T ON CTE.TeamID = T.TeamID
+GROUP BY CTE.TeamID, T.[Name]
+)
+SELECT *
+FROM CTE2
+WHERE CTE2.[Name] LIKE @TeamName
+		AND CTE2.Wins BETWEEN @WinsMin AND @WinsMax
+		AND CTE2.Losses BETWEEN @LossesMin AND @LossesMax
+		AND CTE2.PPGScored BETWEEN @PPGScoredMin AND @PPGScoredMax
+		AND CTE2.PPGAgainst BETWEEN @PPGAgainstMin AND @PPGAgainstMax
+		AND CTE2.GamesPlayed BETWEEN @GamesPlayedMin AND @GamesPlayedMax
+ORDER BY CTE2.Wins DESC;
